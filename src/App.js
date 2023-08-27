f7dd733e0e15f0d7b5013a0fd4f7a1589af4f82a
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-08-27 22:47:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import About from './About'
+
+export default function App() {
+  return (
+
+<>
+  
+  <Hero/>
+  <Navbar/>
+  <About/>
+</>
+  
+
+  )
+}
+
+
+
+
+
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        <div className="navbar">
+        <h2>NFTium</h2>
+        <ul>
+          <li>Collection</li>
+          <li>Prices</li>
+          <li>Support</li>
+        </ul>
+      </div>
+    }
+}
+
+export default Navbar;
